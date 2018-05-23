258391c9c5d1e429ac45e23a0edcfe57504db770
Sunil Tiwari <sunil28071987@gmail.com>
2018-05-23 11:57:23 -0700

    Update 451. Sort Characters By Frequency.java

--- LeetCode-TopInterviewQuestions/451. Sort Characters By Frequency.java
+++ LeetCode-TopInterviewQuestions/451. Sort Characters By Frequency.java
@@ -47,4 +47,52 @@ class Node implements Comparable<Node>{
 
 ===========================================Solution 2 =====================================================
     
+    class Solution {
+    public String frequencySort(String s) { 
+        StringBuilder result = new StringBuilder();
+        Map<Character,Node> map = new HashMap<Character,Node>();
+        for(int i=0; i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                Node n = map.get(s.charAt(i));
+                n.sb.append(Character.toString(s.charAt(i)));
+                n.count=n.count+1;
+            } else {
+                Node n = new Node(Character.toString(s.charAt(i)),1);
+                map.put(s.charAt(i),n);
+            }
+        }
+        
+        NodeCompare nc = new NodeCompare();
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(10,nc);
+        for(Map.Entry<Character,Node> entry : map.entrySet()){
+            pq.add(entry.getValue());
+        }
+                            
+        while(!pq.isEmpty()){
+            Node n = pq.poll();
+            result.append(n.sb.toString());
+        }   
+                            
+        return result.toString();
+    } 
+  }
+                            
+                            
+class Node{
+    public StringBuilder sb;
+    public int count;
     
+    public Node(String str, int count){
+        this.sb=new StringBuilder(str);
+        this.count = count;
+    }    
+} 
+
+class NodeCompare implements Comparator<Node>{
+
+    @Override
+    public int compare(Node n1,Node n2){
+        return n2.count-n1.count;
+    }
+
+}

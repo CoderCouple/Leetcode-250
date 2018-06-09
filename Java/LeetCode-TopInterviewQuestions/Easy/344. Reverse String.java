f78ac2aad3eda21982889a0cf1424fcf3e4c3aef
Sunil Tiwari <sunil28071987@gmail.com>
2018-06-08 20:14:22 -0700

    Update 344. Reverse String.java

--- Java/LeetCode-TopInterviewQuestions/Easy/344. Reverse String.java
+++ Java/LeetCode-TopInterviewQuestions/Easy/344. Reverse String.java
@@ -6,3 +6,19 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+====================================Solution 2=======================================
+    
+    
+    class Solution {
+    public String reverseString(String s) {
+        char [] ch = s.toCharArray();
+        for(int i=0,j=ch.length-1;i<(ch.length/2);i++,j--){
+            char c = ch[i];
+            ch[i]=ch[j];
+            ch[j]=c;
+        }
+        return String.valueOf(ch);
+    }
+}
